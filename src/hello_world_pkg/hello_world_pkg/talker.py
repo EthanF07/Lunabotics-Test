9aea091c853602b9ef306a3c53c0f2268b9dcb6f
EthanF07 <ethanfang50@gmail.com>
2025-10-18 23:14:16 +0000

    test

---
+++ src/hello_world_pkg/hello_world_pkg/talker.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from hello_world_pkg.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/hello_world_pkg/navigation/path_decision.cpp
@@ -0,0 +1,41 @@
+// NOTHING IS FINAL OR BALANCED, conceptual code for now about the logic flow for the robot to choose a 2D path to take
+
+#include "iostream"
+#include "optional"
+
+// Temp class to demonstrate Paths with cost variables tied to them
+// Likely will have a provided Path object from Algorithm later
+class Path {
+private:
+    int Cost;
+
+public:
+    Path(int cost){
+        Cost = cost;
+    }
+
+    int Get_Cost(){
+        return Cost;
+    }
+};
+
+Path path1(2);
+Path path2(3);
+Path path3(99);
+
+Path map_list[] = {path1, path2, path3};
+
+// Choosing the path based on lowest cost value
+Path chosen_path(Path map_list[]) {
+    Path ideal_path();
+    int lowest_cost = 99999;
+    for (int i = 0; i<=3; i++){
+        if (map_list[i].Get_Cost() < lowest_cost){
+            ideal_path() = map_list[i];
+        }
+        else {
+            
+        }
+    }
+    return ideal_path();
+};
